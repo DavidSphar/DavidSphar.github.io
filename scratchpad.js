fd0fc12c742e55204c2b32811002fb4601776b02
DADS-LAPTOP\David <david.sphar@gmail.com>
2017-03-03 11:55:22 -0700

    working css file

--- scratchpad.js
+++ scratchpad.js
@@ -1,6 +1,24 @@
 
+var body = document.body;
+
 function setupBasicPage() {
-	document.title = "Dsphar Javascript Page";
+	document.title = 'Dsphar Javascript Page';
+	document.write('<link rel="stylesheet" type="text/css" href="style.css">');
+	var div = document.createElement('div');
+	div.setAttribute('id','top');
+	var p = document.createElement('p');
+	p.innerHTML="Hello World!";
+	div.appendChild(p);
+	body.appendChild(div);
 }
 setupBasicPage();
+
+function buildFormulaDiv(divId){
+	var div = document.createElement('div');
+	div.setAttribute('class','formulaTop');
+	div.setAttribute('id',divId);
+	body.appendChild(div);
+	div.innerHTML=divId;
+}
+buildFormulaDiv('Fib');
 
